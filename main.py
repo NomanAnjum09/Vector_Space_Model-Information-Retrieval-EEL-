30c95bee5f06b04fdd9d1adee20cf89d4883db67
Noman Anjum <39652150+NomanAnjum09@users.noreply.github.com>
2020-05-17 22:34:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import eel
 from nltk.stem import WordNetLemmatizer 
 import math  
 from nltk.stem.porter import *
-from PyQt5 import QtCore, QtGui, QtWidgets
 import nltk
 import json
 nltk.download('wordnet')
